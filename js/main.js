1fe25d118ab82f57f053d221af65e49db621a432
renatogual <renatogual@hotmail.com>
2020-06-20 14:48:20 -0300

    criação de uma aplicação para consultar cep

--- js/main.js
+++ js/main.js
@@ -1,71 +1,29 @@
-function clicou(){
-    document.getElementById("agradecimento").innerHTML = "Obrigado por clicar"
+//Função consultaCep é chamada quando é clicado o botão de buscar no script html
+function consultaCep(){
+    $(".barra-progresso").show();
+    var cep = document.getElementById("cep").value;
+    var url = "https://viacep.com.br/ws/"+ cep + "/json/";
+    console.log(url); //serve apenas para fazer debugg no navegador
+    
+    //Função ajax para obter os dados da api da url viacep
+    $.ajax({
+        url: url,
+        type: "GET",
+        success: function(response){
+            console.log(response); //serve apenas para fazer debugg no navegador
+            $("#logradouro").html(response.logradouro)
+            $("#bairro").html(response.bairro)
+            $("#localidade").html(response.localidade)
+            $("#uf").html(response.uf)
+            $("#titulo_cep").html("CEP "+response.cep)
+            $(".cep").show();
+            $(".barra-progresso").hide();
+        }
+    })
 }
 
-function redirecionar(){
-    window.open("https://github.com/renatogual/Curso_JavaScript")
-}
-
-function trocar(){
-    alert("Trocar texto")
-}
-/*function soma(n1,n2){
-    return n1 + n2
-}
-
-function validaIdade(idade){
-    if (idade >= 18){
-        validar = true
-    }else{
-        validar = false
-    }
-    return validar
-}
-var idade = prompt("Digite sua idade: ")
-console.log(validaIdade(idade))*/
-
-/*var d = new Date()
-alert(d.getDate())
-*/
-/*var count
-for(count=0; count <=5; count++){
-    alert(count)
-}*/
-
-/*var count = 0
-while (count <= 5){
-    console.log(count)
-    count ++
-}*/
-
-
-/*var idade = prompt("Qual sua idade?")
-if (idade >= 18){
-    alert("Maior de idade")
-}else{
-    alert("Menor idade")
-}*/
-
-/*var frutas = [{nome:"maça", cor:"vermelha"},{nome:"uva", cor:"roxa"}]
-console.log(frutas)
-alert(frutas[1].nome) */
-
-/*var fruta = {nome:"maça", cor:"vermelha"}
-console.log(fruta)
-alert(fruta.cor) */
-
-//var lista = ["maça","pera","laranja"]
-
-//lista.pop()
-//lista.push("uva")
-//console.log(lista.join(" - "))
-
-//var nome = "Renato Gualberto"
-//var idade = 26
-//var idade2 = 10
-//var frase = "Japão é o melhor time do mundo"
-//alert(nome + " tem " + idade + " anos")
-//alert(idade + idade2)
-//console.log(idade)
-//console.log(idade + idade2)
-//console.log(frase.replace("Japão", "Brasil"))
+//Função para carregar a tabela e a barra de carregamento com as informações somente após clicar no botão
+$(function(){
+    $(".cep").hide();
+    $(".barra-progresso").hide();
+})
