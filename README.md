5ab43af49aa1e05945db0806fdce0286e436d703
renatogual <renatogual@hotmail.com>
2020-06-20 00:05:00 -0300

    praticando alguns comandos

---
+++ README.md
@@ -0,0 +1,2 @@
+# Curso_JavaScript
+Repositório para prática de desenvolvimento web com JavaScript do curso Desenvolvedor Full Stack Python da Digital Innovation One
---
+++ js/main.js
@@ -0,0 +1,71 @@
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "Obrigado por clicar"
+}
+
+function redirecionar(){
+    window.open("https://github.com/renatogual/Curso_JavaScript")
+}
+
+function trocar(){
+    alert("Trocar texto")
+}
+/*function soma(n1,n2){
+    return n1 + n2
+}
+
+function validaIdade(idade){
+    if (idade >= 18){
+        validar = true
+    }else{
+        validar = false
+    }
+    return validar
+}
+var idade = prompt("Digite sua idade: ")
+console.log(validaIdade(idade))*/
+
+/*var d = new Date()
+alert(d.getDate())
+*/
+/*var count
+for(count=0; count <=5; count++){
+    alert(count)
+}*/
+
+/*var count = 0
+while (count <= 5){
+    console.log(count)
+    count ++
+}*/
+
+
+/*var idade = prompt("Qual sua idade?")
+if (idade >= 18){
+    alert("Maior de idade")
+}else{
+    alert("Menor idade")
+}*/
+
+/*var frutas = [{nome:"maça", cor:"vermelha"},{nome:"uva", cor:"roxa"}]
+console.log(frutas)
+alert(frutas[1].nome) */
+
+/*var fruta = {nome:"maça", cor:"vermelha"}
+console.log(fruta)
+alert(fruta.cor) */
+
+//var lista = ["maça","pera","laranja"]
+
+//lista.pop()
+//lista.push("uva")
+//console.log(lista.join(" - "))
+
+//var nome = "Renato Gualberto"
+//var idade = 26
+//var idade2 = 10
+//var frase = "Japão é o melhor time do mundo"
+//alert(nome + " tem " + idade + " anos")
+//alert(idade + idade2)
+//console.log(idade)
+//console.log(idade + idade2)
+//console.log(frase.replace("Japão", "Brasil"))
