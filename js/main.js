3eb8ceece73fd1f63afce7ff7d92fab0072fa420
renatogual <renatogual@hotmail.com>
2020-06-23 00:02:28 -0300

    remoção da barra de navegação

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 //Função consultaCep é chamada quando é clicado o botão de buscar no script html
 function consultaCep(){
-    $(".barra-progresso").show();
     var cep = document.getElementById("cep").value;
     var url = "https://viacep.com.br/ws/"+ cep + "/json/";
     console.log(url); //serve apenas para fazer debugg no navegador
@@ -17,13 +16,12 @@ function consultaCep(){
             $("#uf").html(response.uf)
             $("#titulo_cep").html("CEP "+response.cep)
             $(".cep").show();
-            $(".barra-progresso").hide();
         }
     })
 }
 
-//Função para carregar a tabela e a barra de carregamento com as informações somente após clicar no botão
+//Função para carregar a tabela com as informações somente após clicar no botão
 $(function(){
     $(".cep").hide();
-    $(".barra-progresso").hide();
+
 })
